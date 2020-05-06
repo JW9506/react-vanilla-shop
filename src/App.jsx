6e5c33e5c2dd8b5a79ffdfd5e0fade0161e6cc90
JW9506 <cf.dota@gmail.com>
2020-05-06 10:45:27 -0400

    chore: app entry render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,72 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Shop from "./components/Shop"
+import Cart from "./components/Cart"
+import _Axios from "axios"
 
+const Axios = _Axios.create({
+  baseURL: "http://localhost:3001",
+})
 const App = () => {
-  return <div className="App">Hello</div>
+  const [shopItems, setShopItems] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [cartItems, setCartItems] = useState([])
+
+  const handleAddToCart = (itemId: string) => {
+    const shopItem = shopItems.find((shopItem) => shopItem.id === itemId)
+    const cartItem = cartItems.find((cartItem) => cartItem.id === itemId)
+    if (!cartItem) {
+      setCartItems((cartItems) =>
+        cartItems.concat({
+          ...shopItem,
+          quantity: 1,
+        })
+      )
+    } else {
+      setCartItems((cartItems) =>
+        cartItems.map((cartItem) =>
+          cartItem.id === itemId
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        )
+      )
+    }
+  }
+
+  const handleRemoveOneFromCart = (itemId: string) => {
+    const cartItemToRemove = cartItems.find(
+      (cartItem) => cartItem.id === itemId
+    )
+    if (cartItemToRemove.quantity > 1) {
+      setCartItems((cartItems) =>
+        cartItems.map((cartItem) =>
+          cartItem.id === itemId
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+        )
+      )
+    } else {
+      setCartItems((cartItems) =>
+        cartItems.filter((cartItem) => cartItem.id !== itemId)
+      )
+    }
+  }
+
+  return (
+    <div className="App">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Shop shopItems={shopItems} handleAddToCart={handleAddToCart} />
+      )}
+      <div>--------------</div>
+      <Cart
+        cartItems={cartItems}
+        handleAddToCart={handleAddToCart}
+        handleRemoveOneFromCart={handleRemoveOneFromCart}
+      />
+    </div>
+  )
 }
 
 export default App
